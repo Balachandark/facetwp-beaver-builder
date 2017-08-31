0fb27be7823b9c6a643fbfaf6817fe72b169d610
mgibbs189 <mgibbs189@gmail.com>
2017-08-31 08:29:03 -0400

    Typo fix

--- modules/facet/includes/frontend.php
+++ modules/facet/includes/frontend.php
@@ -1,6 +1,6 @@
 <?php
 
-echo '<div class="facetwp-bb-mpodule">';
+echo '<div class="facetwp-bb-module">';
 if ( ! empty( $settings->title ) ) {
 	echo '<h4 class="facettp-facet-title">' . esc_html( $settings->title ) . '</h4>';
 }
